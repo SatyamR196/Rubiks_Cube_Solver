0f204cb39a2c71cd7c1b36df172f6021f0cf4f85
Satyam Rahangdale <satyamrahangdale196@gmail.com>
2025-07-15 10:09:52 +0530

    added initial files

---
+++ Rubiks_Cube_Models/rubiksCube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by satya on 15-07-2025.
+//
+
+#include "rubiksCube.h"
---
+++ Rubiks_Cube_Models/rubiksCube.h
@@ -0,0 +1,8 @@
+//
+// Created by satya on 15-07-2025.
+//
+
+#ifndef RUBIKSCUBE_H
+#define RUBIKSCUBE_H
+
+#endif //RUBIKSCUBE_H
